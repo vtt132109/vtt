94edfda3c717837ee332916b6f2c37a1f4ba70d1
vtt132109 <136872370+vtt132109@users.noreply.github.com>
2025-07-14 22:09:26 +0700

    Update player.js

--- server/player.js
+++ server/player.js
@@ -4,17 +4,16 @@ const CONSTANTS = require('./constants');
 function createPlayer(id, username) {
     return {
         id: id,
-        username: username, // Thuộc tính này đã có, đảm bảo nó được gửi đi
-        x: Math.floor(Math.random() * (CONSTANTS.MAP_WIDTH - 100)) + 50,
-        y: Math.floor(Math.random() * (CONSTANTS.MAP_HEIGHT - 100)) + 50,
+        username: username,
+        x: Math.floor(Math.random() * (CONSTANTS.MAP_WIDTH - 200)) + 100,
+        y: Math.floor(Math.random() * (CONSTANTS.MAP_HEIGHT - 200)) + 100,
         radius: CONSTANTS.PLAYER_RADIUS,
         color: `hsl(${Math.random() * 360}, 100%, 50%)`,
         angle: 0,
         speed: CONSTANTS.PLAYER_SPEED,
-        // Thêm lại các thuộc tính này
-        weapon: 'pistol', // Bắt đầu với súng lục
+        health: CONSTANTS.PLAYER_HEALTH,
+        weapon: 'pistol',
         lastShotTime: 0,
-        health: 100, // Thêm lại máu để nhặt item
     };
 }
 
