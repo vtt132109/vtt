f6c75af7ce0c93433f10cae04d06aa1a6dad0b10
vtt132109 <136872370+vtt132109@users.noreply.github.com>
2025-07-14 23:01:38 +0700

    Update weapon.js

--- server/weapon.js
+++ server/weapon.js
@@ -4,26 +4,26 @@ module.exports = {
       name: 'Pistol',
       damage: 10,
       speed: 12,
-      fireRate: 400, // Cooldown giữa các phát bắn
+      fireRate: 400,
       bulletCount: 1,
       spread: 0.1,
     },
     shotgun: {
       name: 'Shotgun',
-      damage: 8, // Giảm damage một chút vì dễ trúng hơn
+      damage: 8,
       speed: 10,
       fireRate: 1000,
-      bulletCount: 3, // THAY ĐỔI: Bắn 3 viên
+      bulletCount: 3,
       spread: 0.4,
     },
     machinegun: {
       name: 'Machine Gun',
       damage: 7,
       speed: 15,
-      fireRate: 1200, // THAY ĐỔI: Cooldown giữa các ĐỢT bắn
-      bulletCount: 1,  // Bắn 1 viên mỗi lần trong đợt
-      burstCount: 5,   // THAY ĐỔI: Mỗi đợt bắn 5 viên
-      burstDelay: 80,  // THAY ĐỔI: Độ trễ giữa các viên trong một đợt
+      fireRate: 1200,
+      bulletCount: 1,
+      burstCount: 5,
+      burstDelay: 80,
       spread: 0.25,
     }
 };
