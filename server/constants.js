2bb83dd1d01887ec76c558a02b992c40eccdd83f
vtt132109 <136872370+vtt132109@users.noreply.github.com>
2025-07-14 22:06:37 +0700

    Update constants.js

--- server/constants.js
+++ server/constants.js
@@ -1,7 +1,9 @@
 // server/constants.js
 module.exports = {
-    MAP_WIDTH: 800,
-    MAP_HEIGHT: 600,
+    MAP_WIDTH: 1600, // Tăng kích thước bản đồ
+    MAP_HEIGHT: 1200,
     PLAYER_RADIUS: 15,
     PLAYER_SPEED: 4,
+    PLAYER_HEALTH: 100,
+    HEALTH_PACK_AMOUNT: 50,
 };
