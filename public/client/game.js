90920692ce72adac7438da0499f762ba1cf7af11
vtt132109 <136872370+vtt132109@users.noreply.github.com>
2025-07-14 22:11:28 +0700

    Update game.js

--- public/client/game.js
+++ public/client/game.js
@@ -13,47 +13,94 @@ export class Game {
         this.state = {
             players: {},
             bullets: {},
+            walls: [],
+            items: {},
             selfId: null,
         };
+        this.INTERPOLATION_FACTOR = 0.15;
     }
 
     start(username) {
         this.network.connect();
         this.network.joinGame(username);
         this.input.startListening();
+        this.resizeCanvas();
+        window.addEventListener('resize', () => this.resizeCanvas());
         requestAnimationFrame(() => this.gameLoop());
+    }
+
+    resizeCanvas() {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
     }
 
     gameLoop() {
         const self = this.state.players[this.state.selfId];
-        if (self && this.input.state.mousePos) {
+        if (self) {
             this.input.state.angle = Math.atan2(
-                this.input.state.mousePos.y - self.y,
-                this.input.state.mousePos.x - self.x
+                this.input.state.mousePos.y - this.canvas.height / 2,
+                this.input.state.mousePos.x - this.canvas.width / 2
             );
         }
         
         this.network.sendInput(this.input.getState());
+        this.interpolateEntities();
         this.renderer.draw(this.state);
         requestAnimationFrame(() => this.gameLoop());
     }
 
+    interpolateEntities() {
+        for (const id in this.state.players) {
+            const player = this.state.players[id];
+            if (player.renderX) {
+                player.renderX += (player.x - player.renderX) * this.INTERPOLATION_FACTOR;
+                player.renderY += (player.y - player.renderY) * this.INTERPOLATION_FACTOR;
+            }
+        }
+    }
+
     setInitialState(state) {
         this.state.selfId = this.network.socket.id;
-        this.state.players = state.players;
+        this.state.walls = state.walls;
+        this.state.items = state.items;
         this.state.bullets = state.bullets;
+        for (const id in state.players) {
+            const player = state.players[id];
+            player.renderX = player.x;
+            player.renderY = player.y;
+            this.state.players[id] = player;
+        }
     }
     
     updateState(state) {
-        this.state.players = state.players;
+        for (const id in state.players) {
+            if (this.state.players[id]) {
+                Object.assign(this.state.players[id], state.players[id]);
+            } else {
+                const player = state.players[id];
+                player.renderX = player.x;
+                player.renderY = player.y;
+                this.state.players[id] = player;
+            }
+        }
         this.state.bullets = state.bullets;
     }
 
     addPlayer(player) {
+        player.renderX = player.x;
+        player.renderY = player.y;
         this.state.players[player.id] = player;
     }
 
     removePlayer(id) {
         delete this.state.players[id];
+    }
+
+    updateItem(id, isActive) {
+        if (this.state.items[id]) this.state.items[id].active = isActive;
+    }
+
+    addItem(item) {
+        if (item) this.state.items[item.id] = item;
     }
 }
