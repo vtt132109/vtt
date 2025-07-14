c456fdac21f8d5f89aec7dbc0cfd5405508bfc11
vtt132109 <136872370+vtt132109@users.noreply.github.com>
2025-07-14 22:04:50 +0700

    Update weapon.js

--- server/weapon.js
+++ server/weapon.js
@@ -1,8 +1,25 @@
 // server/weapon.js
 module.exports = {
-    basic: {
+    pistol: {
+      name: 'Pistol',
       damage: 10,
+      speed: 12,
+      fireRate: 400, // 400ms cho mỗi phát bắn
+    },
+    shotgun: {
+      name: 'Shotgun',
+      damage: 15,
       speed: 10,
-      // fireRate: 0, // Bắn ngay lập tức khi có tín hiệu
+      fireRate: 1000,
+      spread: 0.5,
+      bulletCount: 6,
+    },
+    machinegun: {
+      name: 'Machine Gun',
+      damage: 5,
+      speed: 15,
+      fireRate: 100,
+      spread: 0.2,
+      bulletCount: 1,
     }
 };
