00d0a11a4b89c73518438b44fb4678069dfc1667
vtt132109 <136872370+vtt132109@users.noreply.github.com>
2025-07-14 21:47:53 +0700

    Update network.js

--- public/client/network.js
+++ public/client/network.js
@@ -1,7 +1,7 @@
 // public/client/network.js
 export class Network {
     constructor(game) {
-        this.socket = io(); // Thay đổi URL khi deploy
+        this.socket = io();
         this.game = game;
     }
 
@@ -10,15 +10,8 @@ export class Network {
         this.socket.on('gameState', (state) => this.game.updateState(state));
         this.socket.on('newPlayer', (player) => this.game.addPlayer(player));
         this.socket.on('playerDisconnected', (id) => this.game.removePlayer(id));
-        this.socket.on('destructibleDestroyed', (id) => this.game.updateDestructible(id, false));
-        this.socket.on('destructibleRespawned', (id) => this.game.updateDestructible(id, true));
-        this.socket.on('itemPickedUp', (id) => this.game.updateItem(id, false));
-        this.socket.on('newItem', (item) => this.game.addItem(item));
-        this.socket.on('chatMessage', (data) => this.game.renderer.ui.addChatMessage(data));
     }
 
-    joinGame(username) { this.socket.emit('joinGame', { username }); }
+    joinGame(username) { this.socket.emit('joinGame', username); }
     sendInput(input) { this.socket.emit('playerInput', input); }
-    sendDash() { this.socket.emit('dash'); }
-    sendChatMessage(msg) { this.socket.emit('chatMessage', msg); }
 }
