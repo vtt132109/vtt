77bfe85241ed10e7dd6229045c8b78e03ad10824
vtt132109 <136872370+vtt132109@users.noreply.github.com>
2025-07-14 22:34:11 +0700

    Update constants.js

--- server/constants.js
+++ server/constants.js
@@ -6,5 +6,11 @@ module.exports = {
     PLAYER_SPEED: 4,
     PLAYER_HEALTH: 100,
     HEALTH_PACK_AMOUNT: 50,
-    HOMING_DURATION: 5000, // 5 giây cho đạn tầm nhiệt
+    HOMING_DURATION: 5000,
+    // MỚI
+    GRENADE_FUSE_TIME: 3000, // 3 giây nổ
+    GRENADE_RADIUS: 80,
+    GRENADE_DAMAGE: 60,
+    GRENADE_COOLDOWN: 8000, // 8 giây hồi
+    BULLET_MAX_BOUNCES: 2, // Số lần nảy tối đa
 };
